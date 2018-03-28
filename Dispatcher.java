3d3f804f6566db9b8135bf022aa20d5529e80d43
wcirucci <37445321+wcirucci@users.noreply.github.com>
2018-03-28 10:26:57 -0400

    Update Dispatcher.java

--- Dispatcher.java
+++ Dispatcher.java
@@ -6,5 +6,39 @@ package EMSSystem;
  *
  */
 public class Dispatcher {
+//location (x, y) positive values are roads on grid
+	
+	protected int size;
+	protected boolean active;
+	protected int location;
+	protected int homelocation;
+	protected boolean destination;
+	
+	
+	public dispatch()
+	{
+		size=1;
+		active=false;
+		location=0;
+		homelocation=0;
 
+		if (active=true)
+		if (location<event.location)
+		while (location<event.location)
+		location++;
+		else
+		if(location>event.location)
+		while(location>event.location)
+		Location--;
+		if(location=event.location)
+		destination=true;
+
+		if(active=false && location !=homelocation)
+		if(location<homelocation)
+		location++;
+		else if(location>homelocation)
+		location--;
+
+
+	}
 }
