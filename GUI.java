767bf86dc2e49b6ee7d09ff9a483769f08dfa701
RyanLMn <34321690+RyanLMn@users.noreply.github.com>
2018-04-13 08:59:44 -0400

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,41 @@
+package EMSSystem;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+
+public class GUI {
+	private static JFrame frame;
+	private static Container contentPane;
+	private static JList events;
+	private GUIMap map;
+	
+	public GUI() {
+		events = new JList();
+		
+		map = new GUIMap(25, 25);
+		map.setBackground(Color.LIGHT_GRAY);
+		makeFrame();
+	}
+	
+	public void makeFrame() {
+		frame = new JFrame("EMS System");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		contentPane = (JPanel) frame.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		
+		JScrollPane scrollPane = new JScrollPane(events);
+		scrollPane.setPreferredSize(new Dimension(200, 200));
+		
+		frame.setPreferredSize(new Dimension(900, 700));
+		
+		contentPane.add(scrollPane, BorderLayout.WEST);
+		contentPane.add(map, BorderLayout.CENTER);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
---
+++ GUIMap.java
@@ -0,0 +1,57 @@
+package EMSSystem;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class GUIMap extends JPanel{
+	private int w;
+	private int h;
+	private final int m = 25;
+
+	private Dimension size;
+	
+	public GUIMap(int width, int height) {
+		w = width;
+		h = height;
+		size = new Dimension(0,0);
+	}
+	
+	@Override
+	public void paintComponent (Graphics g) {
+		ArrayList<ArrayList<Building>> buildings = Map.getBuilding();
+		for (int x=0; x < buildings.size(); x++) {
+			for (int y=0; y < buildings.get(x).size(); y++) {
+				Building B = buildings.get(x).get(y);
+				int X = x;
+				int Y = y;
+				X *= m;
+				Y *= m;
+				if (B instanceof House) {
+					g.setColor(Color.WHITE);
+					g.fillRect(X, Y, m, m);
+				} else if (B instanceof Roads) {
+					g.setColor(Color.BLACK);
+					g.fillRect(X, Y, m, m);
+				} else if (B instanceof EMT) {
+					g.setColor(Color.GREEN);
+					g.fillRect(X, Y, m, m);
+				} else if (B instanceof FireDept) {
+					g.setColor(Color.RED);
+					g.fillRect(X, Y, m, m);
+				} else if (B instanceof Hospital) {
+					g.setColor(Color.ORANGE);
+					g.fillRect(X, Y, m, m);
+				} else {
+					g.setColor(Color.BLUE);
+					g.fillRect(X, Y, m, m);
+				}
+			}
+		}
+	}
+
+	public Dimension setPreferredSize(int i, int j) {
+		// TODO Auto-generated method stub
+		return new Dimension(w,h);
+	}
+}
